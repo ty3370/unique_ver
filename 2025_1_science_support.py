6e81f00f010d93ed9d497dd588c171f47cbdeaec
ty3370 <77711112+ty3370@users.noreply.github.com>
2025-06-21 05:36:20 +0900

    Add files via upload

--- 2025_1_science_support.py
+++ 2025_1_science_support.py
@@ -383,14 +383,10 @@ def page_2():
         ❗AI의 응답은 부정확할 수 있으므로, 정확한 정보는 선생님께 확인하세요.
         """)
     col1, col2 = st.columns([1, 1])
-    with col1:
-        if st.button("이전"):
-            # 로그인 정보 초기화
-            st.session_state.pop("user_number", None)
-            st.session_state.pop("user_name", None)
-            st.session_state.pop("user_code", None)
-            st.session_state["step"] = 1
-            st.rerun()
+#    with col1:
+#        if st.button("이전"):
+#            st.session_state["step"] = 1
+#            st.rerun()
     with col2:
         if st.button("다음"):
             st.session_state["step"] = 3
